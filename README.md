b125613dd267fbea572c6f67f4676645a4ad328f
Alina <goaga.alina@gmail.com>
2020-07-03 11:50:17 +0100

    user can read, create and update files

--- README.md
+++ README.md
@@ -3,11 +3,17 @@
 Files can be read and created via the command line. 
 `http.FileServer` can be used to display the contents of your store and files in the browser.
 
-To start server: 
+To start the server and set up the store where the files that you'll work with are located: 
  `go run main.go --store=./tmp`
  
 Visit localhost to see the contents of your store
  `http://localhost:8000/`
 
-To see the contents of a file in the terminal (where file2.txt is the name of the file)
-`go run main.go --action=read --file=file2.txt`
+See the contents of a file in the terminal (where file2.txt is the name of the file):
+`go run main.go --store=./tmp --action=read --file=file1.txt`
+
+Create a new file and write to it:
+`go run main.go --store=./tmp --action=create --file=file3.txt --content=some_content`
+
+Update the content of a file (this will overwrite any content that is already present in the file)
+`go run main.go --store=./tmp --action=update --file=file3.txt --content=some_other_content`
--- main.go
+++ main.go
@@ -6,26 +6,62 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 var store = flag.String("store", "", "Location of the files")
 var action = flag.String("action", "", "Action over the file")
 var file = flag.String("file", "", "New file name")
+var content = flag.String("content", "", "Content to be added to the file")
 
 func readFile() {
 	data, err := ioutil.ReadFile(*store + "/" + *file)
 	if err != nil {
 		log.Panicf("failed reading data from file: %s", err)
 	}
-	fmt.Printf("\nData: %s", data)
+	fmt.Printf("\nData: %s\n", data)
+}
+
+func createFile() {
+	data := []byte(*content)
+	err := ioutil.WriteFile(*store + "/" + *file, data, 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func updateContent() {      
+	file, err := os.OpenFile(
+        *store + "/" + *file,
+        os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+        0666,
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    byteContent := []byte(*content)
+    file.Write(byteContent)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("The file has been succesfully updated")
 }
 
 func handleRequests() {
 	flag.Parse()
 
-	if *action == "read" {
-		readFile()
-	}
+	switch *action {
+		case "read":
+			readFile()
+		case "create":
+			createFile()
+		case "update":
+			updateContent()
+		default:
+			fmt.Println("Please choose an action in the CLI")
+    }
 
 	http.Handle("/", http.FileServer(http.Dir(*store)))
 
